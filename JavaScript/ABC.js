a551ceed4fc8f335cae548d07f44889cf1d0c74e
Dex-22 <enriquesaenz0@gmail.com>
2020-04-14 19:35:33 -0500

    Añadí la carpeta JavaScript

---
+++ JavaScript/ABC.js
@@ -0,0 +1,33 @@
+//Aqui empieza el objeto cartila
+/*var Cartilla = {
+    contenedor: "",
+    color: "",
+    contenido: "",
+    imagen: {}
+};
+function Cartilla(contenedor,color,contenido,imagen,URL){
+    this.contenedor = document.getElementById(contenedor);
+    this.contenido = document.getElementById(contenido).innerHTML;
+    this.imagen.contenedor = document.getElementById(imagen);
+    this.imagen.URL = this.imagen.contenedor.src(URL);
+ }
+ Cartilla.Crear = function () {
+     //this.contenido.appendChild(contenido);
+     this.contenedor.appendChild(this.imagen.contenedor);
+     this.contenedor.appendChild(this.contenido);
+      };
+
+var A = new Cartilla(Contenedor1,Contenido1,Imagen1,"T1E1.jpeg");
+A.Crear();
+*/
+
+
+
+/*{
+//  contenedor : ,
+  contenido : this.contenido,
+  fondo : document.getElementById('Cartilla1').style.background,
+  imagen : document.getElementById('Imagen1').src,
+  titulo : document.getElementById('Titulo1'),
+//  boton : boton
+};*/
